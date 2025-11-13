e80be479358abe87b471b9de8c827726647cb69e
GitHub Action <action@github.com>
2025-11-13 17:24:08 +0000

    Format: Aplicar formateo Black automático [skip ci]

--- matching/adapters.py
+++ matching/adapters.py
@@ -55,9 +55,7 @@ class RestrictedSocialAccountAdapter(DefaultSocialAccountAdapter):
                     "Solo se permiten cuentas con email institucional de @davinci.edu.ar",
                 )
                 # Redirigir al login con mensaje de error
-                raise ImmediateHttpResponse(
-                    HttpResponseRedirect("/matching/login/")
-                )
+                raise ImmediateHttpResponse(HttpResponseRedirect("/matching/login/"))
 
     def is_open_for_signup(self, request, sociallogin):
         """Permitir registro solo con emails válidos."""
@@ -66,4 +64,3 @@ class RestrictedSocialAccountAdapter(DefaultSocialAccountAdapter):
             domain = email.split("@")[-1].lower()
             return domain in self.ALLOWED_DOMAINS
         return False
-
