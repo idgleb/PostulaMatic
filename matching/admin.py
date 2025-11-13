2cb13684e176042436c67ee9717e30ab157d4eb2
GitHub Action <action@github.com>
2025-11-13 20:46:46 +0000

    Format: Aplicar formateo isort automático [skip ci]

--- matching/admin.py
+++ matching/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import ApplicationAttempt, JobPosting, MatchScore, UserCV, UserProfile
+from .models import (ApplicationAttempt, JobPosting, MatchScore, UserCV,
+                     UserProfile)
 
 
 @admin.register(UserProfile)
--- matching/management/commands/test_session_simple.py
+++ matching/management/commands/test_session_simple.py
@@ -4,9 +4,8 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from matching.clients.dvcarreras_playwright_flaresolverr import (
-    DVCarrerasPlaywrightFlareSolverr,
-)
+from matching.clients.dvcarreras_playwright_flaresolverr import \
+    DVCarrerasPlaywrightFlareSolverr
 
 logger = logging.getLogger(__name__)
 
